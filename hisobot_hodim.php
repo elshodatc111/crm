49fc1b58b62a94af42e3e54c9f4247469d85f87e
Elshod Musurmonov <elshodatc1116@gmail.com>
2023-11-06 22:36:58 +0500

    Hisobotlar bo'limi tayorlandi

--- hisobot_hodim.php
+++ hisobot_hodim.php
@@ -22,22 +22,27 @@
       <nav>
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="index.php">Home</a></li>
-          <li class="breadcrumb-item active">Kobinet</li>
+          <li class="breadcrumb-item active">Hisobot</li>
         </ol>
       </nav>
+    </div>
+    <div class="row">
+      <div class="col-lg-2 col-4"><a href="./hisobot_tashrif.php" class='btn btn-primary w-100 py-1 my-1' style="border:0;border-radius:0;">TASHRIFLAR</a></div>
+      <div class="col-lg-2 col-4"><a href="./hisobot_guruh.php" class='btn btn-primary w-100 py-1 my-1' style="border:0;border-radius:0;">GURUHLAR</a></div>
+      <div class="col-lg-2 col-4"><a href="./hisobot_moliya.php" class='btn btn-primary w-100 py-1 my-1' style="border:0;border-radius:0;">MOLIYA</a></div>
+      <div class="col-lg-2 col-4"><a href="./hisobot_techer.php" class='btn btn-primary w-100 py-1 my-1' style="border:0;border-radius:0;">O'QITUVCHILAR</a></div>
+      <div class="col-lg-2 col-4"><a href="./hisobot_hodim.php" class='btn btn-success w-100 py-1 my-1' style="border:0;border-radius:0;">HODIMLAR</a></div>
+      <div class="col-lg-2 col-4"><a href="./hisobot.sms.php" class='btn btn-primary w-100 py-1 my-1' style="border:0;border-radius:0;">SMS</a></div>
     </div>
     <section class="section dashboard">
       <div class="row">
         <div class="card info-card sales-card">
           <div class="card-body">
-            <h5 class="card-title w-100 text-center pb-0 mb-1">Kobinet</h5>
+            <h5 class="card-title w-100 text-center pb-0 mb-1">Hodimlar</h5>
           </div>
         </div>
       </div>
     </section>
-
-
-
   </main>
   
   <?php include("./check/footer.html");?>
